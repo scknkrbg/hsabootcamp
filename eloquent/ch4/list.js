f605a210380bed00e742ca9e051481436524d4b3
Seckin Karabag <seckinkarabag@Seckins-MacBook-Pro.local>
2017-10-04 16:49:17 +0300

    ch4/ex2 completed

--- eloquent/ch4/list.js
+++ eloquent/ch4/list.js
@@ -1,51 +1,48 @@
 // Your code here.
+function nth(list, number){
+  if(number === 0){
+    return list.value;
+  } else if(list.rest != null) {
+    return nth(list.rest, number-1);
+  } else {
+  return undefined;
+  }
+};
 
-  
-function prepend(prep, rest=null){
+function prepend(value, rest){
   var obj = {};
-  obj.value = prep;
+  obj.value = value;
   obj.rest = rest;
   return obj;
-}
-  
-function nth (list, number){
-  var arr = listToArray(list);
-  return arr[number];
-}
-
-var l = {value:1, rest:{value:2, rest:null}};
-
-console.log(nth(l,1));
-
-function arrayToList(arr){
-  var obj = {};
-  if(arr != null){
-    obj.value = arr[0];
-    return arrayToList(arr.slice(1,));
-                        
-  } 
-  return obj;
-}
-
-console.log(arrayToList([1,2,3]));
+};
 
 function listToArray(list){
   var arr = [];
-  if(list != null){
+  if(list.rest === null){
+    arr.push(list.value);
+    return arr;
+  } else {
     arr.push(list.value);
     return arr.concat(listToArray(list.rest));
   }
-  return arr;
+};
+
+function arrayToList(arr){
+  len = arr.length;
+  if(len === 1){
+    var obj = prepend(arr[0],null);
+    return obj;
+  } else {
+    var obj = prepend(arr[0], arrayToList(arr.slice(1,len)))
+    return obj;
+  }
 }
 
-  
-/*
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-*/
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-//console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
