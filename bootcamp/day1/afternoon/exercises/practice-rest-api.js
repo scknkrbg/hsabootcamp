fdf347271b9da3277c4f7e6a4505c3e95f1e0061
Seckin Karabag <gokbilimci@gmail.com>
2017-11-05 22:36:57 +0300

    ch5 higher-order functions chapter exercises finished

--- bootcamp/day1/afternoon/exercises/practice-rest-api.js
+++ bootcamp/day1/afternoon/exercises/practice-rest-api.js
@@ -33,8 +33,65 @@ var users = [
 // feel free to use any built-in functions (including ES6 functions)
 // don't use any external libraries (no more require() statements)
 
+// get all users
+app.get('/users', (req, res, next) => {
+  console.log(users.length);
+  res.send(users);
+});
 
+// get specific user
+app.get('/users/:id', (req, res, next) => {
+ // console.log(typeof users[0].id, typeof +req.params.id);
+ // console.log(req.params.id, req.body, req.params);
+  for(var i=0; i<users.length; i++){
+    if(users[i].id === +req.params.id){
+      return  res.json(users[i]);
+    }
+  }
+  return res.status(404).send('No user with that ID');
+});
 
+app.post('/users/', (req, res, next) => {
+  if (typeof req.body.name !== 'string')
+    return res.status(400).send('Missing name');
+  if (typeof req.body.email !== 'string')
+    return res.status(400).send('Missing email');
+  if (typeof req.body.password !== 'string')
+    return res.status(400).send('Missing password');
+
+  var newUser = {
+    id : genId(),
+    name : req.body.name,
+    email : req.body.email,
+    password : req.body.password
+  };
+  users.push(newUser);
+  return res.sendStatus(200);
+});
+
+app.put('/users/:id', (req, res, next) => {
+  for (var i = 0; i < users.length; i++) {
+    if(users[i].id === +req.params.id) {
+      users[i].name = req.body.name || users[i].name;
+      users[i].email = req.body.email || users[i].email;
+      users[i].password = req.body.password || users[i].password;
+      return res.sendStatus(200);
+    };
+  }
+  return res.status(404).send('No user with that ID');
+});
+
+    
+
+app.delete('/users/:id', (req, res, next) => {
+  for (var i = 0; i < users.length; i++) {
+    if(users[i].id === +req.params.id) {
+      users.splice(i, 1);
+      return res.sendStatus(200);
+    }
+  }
+  return res.status(404).send('No user with that ID');
+});
 
 // if you finish early, start adding data validation. don't insert values other
 // than name/email/pw, reject creations if they don't have an email and pw, etc.
--- bootcamp/day1/morning/exercises/app.js
+++ bootcamp/day1/morning/exercises/app.js
@@ -8,6 +8,12 @@ var app = express()
 // resulting array should be ['a', 'b', ... 'y', 'z']
 
 // bahahaha i hope this causes a merge conflict so you can practice resolving
+var alphabet = [];
+for(var i=97; i<123; i++){
+  alphabet.push(String.fromCharCode(i));
+}
+
+
 var deleteThis = ['sorry', 'guys']
 for (var i = 0; i < deleteThis.length; i++) {
     console.log(deleteThis[i]);
@@ -21,6 +27,13 @@ for (var i = 0; i < deleteThis.length; i++) {
 // first endpoint should be:
 // app.get('/a', function(req, res) { res.send("1") });
 
+for(var i=0; i<alphabet.length; i++){
+  (function (i){
+    app.get('/'+alphabet[i], function(req, res){res.send(String(i+1))});
+  })(i)
+}
+
+
 
 
 
@@ -32,12 +45,34 @@ for (var i = 0; i < deleteThis.length; i++) {
 // that randomly pairs students. feel free to use whatever data structure you
 // see fit
 
+var st = require("./students");
 
+app.get('/partners', function (req, res) {
+  var std_pairs = function (arr){
+  console.log(arr);
+  for(var i=0; i<100; i++){
+    var a = Math.floor(Math.random()*arr.length);
+    var b = Math.floor(Math.random()*arr.length);
+    var tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+  }
+  console.log(arr);
+  var str = '';
+  for(var i=0; i<arr.length; i++){
+    str += String(arr[i])+'-'+String(arr[i+1])+'<br>';
+    ++i
+  }
+  return str;
+  }
+  res.send(std_pairs(st));
+});
+  
 
 
 app.get('/', function (req, res) {
     res.send('Hello, World!')
-})
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
